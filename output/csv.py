c728baf00845a83d76b22282a0e8208b8f6a97de
j-blake <j-blake@users.noreply.github.com>
2019-08-01 15:00:55 -0500

    windows compatibility

--- output/csv.py
+++ output/csv.py
@@ -7,7 +7,7 @@ from output import Output
 class Csv(Output):
     def get_games_output(self, games: list):
         sorted_games = sorted(games, key=lambda g: g['rating'], reverse=True)
-        with open(os.path.expanduser('~/Desktop/mlb-game-of-the-day.csv'), 'w+')as file:
+        with open(os.path.join(os.path.expanduser('~'), 'Desktop', 'mlb-game-of-the-day.csv'), 'w', newline='') as file:
             writer = csv.DictWriter(file, sorted_games[0].keys())
             writer.writeheader()
             writer.writerows(sorted_games)
